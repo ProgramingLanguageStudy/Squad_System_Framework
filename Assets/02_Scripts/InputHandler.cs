ca3ac0ec8174217c7ee0d1dd06344b15c3110fdf
endsun1234 <endsun1234@naver.com>
2026-01-29 21:56:58 +0900

    플레이어 기본 이동 및 애니메이션 추가

---
+++ Assets/02_Scripts/InputHandler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    // 다른 스크립트에서 읽을 수 있도록 프로퍼티로 노출
+    public Vector2 MoveInput { get; private set; }
+    public bool AttackTriggered { get; private set; } // 나중에 공격 구현 시 사용
+
+    // Player Input 컴포넌트에서 Send Messages 방식으로 호출
+    public void OnMove(InputValue value)
+    {
+        MoveInput = value.Get<Vector2>();
+    }
+
+    public void OnAttack(InputValue value)
+    {
+        // 버튼을 누른 순간만 감지
+        AttackTriggered = value.isPressed;
+    }
+
+    // 로직 처리가 끝난 후 트리거를 초기화해야 할 경우 사용
+    public void ResetAttackTrigger() => AttackTriggered = false;
+}
